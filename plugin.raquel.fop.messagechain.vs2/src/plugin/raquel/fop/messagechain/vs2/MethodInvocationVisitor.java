91eb71c953e5ce4f9107015d211be1b6c8032de7
raquelbromao <raquelbromao@gmail.com>
2016-11-30 10:38:30 -0200

    New class visitor

---
+++ plugin.raquel.fop.messagechain.vs2/src/plugin/raquel/fop/messagechain/vs2/MethodInvocationVisitor.java
@@ -0,0 +1,20 @@
+package plugin.raquel.fop.messagechain.vs2;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+
+public class MethodInvocationVisitor extends ASTVisitor {
+	List<MethodInvocation> methods = new ArrayList<MethodInvocation>();
+
+	public boolean visit(MethodInvocation node) {
+		methods.add(node);
+		//return super.visit(node);
+		return false;
+	}
+
+	public List<MethodInvocation> getExpression() {
+		return methods;
+	}
+}
